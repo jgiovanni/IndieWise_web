b982510bf51f1b1005a2bba0f353d53b88e28603
Jerez Bain <jerez@gorigins.com>
2016-08-11 20:44:01 -0400

    move to alpha p1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" ng-app="IndieWise" class="no-js">
 <head>
     <title>IndieWise</title>
-    <base href="http://52.207.215.154/">
+    <base href="http://52.207.215.154/alpha/">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
