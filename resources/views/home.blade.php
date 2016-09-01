1846dda5129fa4232792d536a3bff0e3306428e9
Jerez Bain <jerez@gorigins.com>
2016-09-01 00:52:10 -0400

    Socket.io for in house notification/events system p8

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,10 +15,10 @@
         </div>
     </div>
 </div>
-<script src="https://192.168.10.10:3000/socket.io/socket.io.js"></script>
+<script src="https://cdn.socket.io/socket.io-1.3.5.js"></script>
 <script>
     //var socket = io('http://localhost:3000');
-    var socket = io('https://192.168.10.10:3000');
+    var socket = io('127.0.0.1:3000');
     socket.on("test-channel:IndieWise\\Events\\EventName", function(message){
         // increase the power everytime we load test route
         $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
