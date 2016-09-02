8120d4ca8539a4e615ffd74b60e3f7e0507ff23e
Jerez Bain <jerez@gorigins.com>
2016-09-01 23:54:40 -0400

    socket.js asset bump

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,8 @@
         </div>
     </div>
 </div>
-{{--<script src="https://cdn.socket.io/socket.io-1.4.5.js"></script>--}}
-<script src="{{ asset('assets/js/socket.js') }}"></script>
+<script src="https://cdn.socket.io/socket.io-1.4.5.js"></script>
+{{--<script src="{{ asset('assets/js/socket.js') }}"></script>--}}
 <script>
 //    var url = window.location.origin;
 //    var url = 'https://getindiewise.com:3000';
