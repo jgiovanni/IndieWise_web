145621b317eefeb6297d1e87de501c5239d412ae
Jerez Bain <jerez@gorigins.com>
2016-09-19 13:13:52 -0400

    bump console 3

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -40,6 +40,6 @@
 <div id="main" class="animate-slide-up" ui-view="main" layout="column"></div>
 <ms-theme-options></ms-theme-options>
 <script src="/admin/scripts/vendor-3df4a11a62.js"></script>
-<script src="/admin/scripts/app-10ef547eb4.js"></script>
+<script src="/admin/scripts/app-25d6cb61e0.js"></script>
 </body>
 </html>
