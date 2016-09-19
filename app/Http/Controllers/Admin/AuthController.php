e060298d8bdbb33f82042e065f0aee5fbf31b656
Jerez Bain <jerez@gorigins.com>
2016-09-19 14:52:05 -0400

    updated console with security pt8

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -86,7 +86,7 @@ class AuthController extends Controller
             }
 
             $user = JWTAuth::user();
-            dd($user);
+            dd($test);
             $test = DB::table('role_user')->where('user_id', $user->id)->where('role_id', 1000)->first();
             if (empty($test)) {
                 return response()->json(['user_not_authorized'], 401);
