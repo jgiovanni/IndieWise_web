96d9895f34a992253b8bb285843d6d9677a0ab0e
Jerez Bain <jerez@gorigins.com>
2016-09-22 14:23:02 -0400

    randomize featured section

--- resources/assets/js/directives.js
+++ resources/assets/js/directives.js
@@ -43,7 +43,7 @@
                 templateUrl: 'directives/featured-area.html',
                 scope: {},
                 link: function (scope, el, attrs) {
-                    DataService.collection("projects").then(function (result) {
+                    DataService.collection("projects", { random: true }).then(function (result) {
                         scope.featuredFilms = result.data;
                         // console.log("featuredFilms: ", scope.featuredFilms);
                     }).then(function () {
