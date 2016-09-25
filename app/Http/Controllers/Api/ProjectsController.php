9b14d69834d2c66a235fa88b70288106eb7bd810
Jerez Bain <jerez@gorigins.com>
2016-09-25 02:47:16 -0400

    watch issue fixed

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -115,8 +115,7 @@ class ProjectsController extends Controller
     public function recordWatched(Request $request)
     {
         $watch = Watch::firstOrCreate(['project_id' => $request->get('project_id')]);
-        $watch->touch();
-        $watch->increment('count');
+        $watch->touch()->increment('count');
 //        DB::table('Watched')->where('project_id', $watch->project_id)->increment('count');
     }
 
