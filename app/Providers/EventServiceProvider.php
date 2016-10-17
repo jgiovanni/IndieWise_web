06159718357c7ca1b87b405dec8f7537e5b2e1b4
Jerez Bain <jerez@gorigins.com>
2016-10-17 11:35:36 -0400

    Laravel upgraded to 5.3 pt6

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace IndieWise\Providers;
 
-//use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -60,7 +59,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //parent::boot();
+        parent::boot();
 
         //
     }
