96823afd5dcefe1131ce4f07b2650f39a557f0f5
Jerez Bain <jerez@gorigins.com>
2016-10-17 11:19:35 -0400

    Laravel upgraded to 5.3 pt4

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace IndieWise\Providers;
 
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
