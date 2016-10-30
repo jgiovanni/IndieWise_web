3f996ce4e5d55a1cb4ef0b60c9d83a8c4f6c2885
Jerez Bain <jerez@gorigins.com>
2016-10-30 08:09:21 -0400

    better packaging and forum updates p3

--- config/chatter.php
+++ config/chatter.php
@@ -90,7 +90,7 @@ return [
 
 //    'master_file_extend' => 'layouts.app',
 //    'master_file_extend' => 'layouts.dev_master',
-    'master_file_extend' => 'layouts.master',
+    'master_file_extend' => 'layouts.forums',
 
     /*
     |--------------------------------------------------------------------------
