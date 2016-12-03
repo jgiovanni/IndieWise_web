9f77c41a93a3bd07e114207e8cffa9240aa04bc6
Jerez Bain <jerez@gorigins.com>
2016-12-03 05:57:16 -0500

    bump

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -113,7 +113,7 @@ class AuthController extends Controller
             $customClaims = [
                 'id' => $user->id,
                 'email' => $user->email,
-                'username' => substr(strrchr($user->email, "@"), 0),
+                'username' => explode("@", $user->email)[0],
                 'firstName' => $user->firstName,
                 'lastName' => $user->lastName,
                 'picture' => $user->avatar,
@@ -140,7 +140,7 @@ class AuthController extends Controller
             $customClaims = [
                 'id' => $user->id,
                 'email' => $user->email,
-                'username' => substr(strrchr($user->email, "@"), 0),
+                'username' => explode("@", $user->email)[0],
                 'firstName' => $user->firstName,
                 'lastName' => $user->lastName,
                 'picture' => $user->avatar,
