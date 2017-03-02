22a0d09f4763a77d23f7b2f7a92c1209bfa8b7b1
Jerez Bain <jerezb31@gmail.com>
2017-03-02 14:53:09 -0500

    filestack v3 library

--- resources/views/shared/_head.blade.php
+++ resources/views/shared/_head.blade.php
@@ -67,18 +67,17 @@
     <link rel="stylesheet" href="{{ elixir('css/all.css') }}"/>
 
     <script>
-        /* Replace 'APP_ID' with your app ID */
         window.intercomSettings = {
             app_id: 'ppp65byn'
         };
-        let APP_ID = "ppp65byn";
 
-        (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+        (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + intercomSettings.app_id;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
 
     </script>
     {{--<script type="text/javascript" src="//cdn.broadstreetads.com/init.js"></script>--}}
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.3/socket.io.min.js"></script>
+    <script src="https://static.filestackapi.com/v3/filestack.js"></script>
 
     @yield('css')
 
