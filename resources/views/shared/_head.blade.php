ae132f8a7d22d601405342f13626d6196a1d8290
Jerez Bain <jerezb31@gmail.com>
2017-04-22 16:19:20 -0400

    add GA tracking codes

--- resources/views/shared/_head.blade.php
+++ resources/views/shared/_head.blade.php
@@ -35,6 +35,16 @@
 
     <script type="text/javascript" src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-27155404-17', 'auto');
+        ga('send', 'pageview');
+
+    </script>
     <script src="//load.sumome.com/" data-sumo-site-id="bd61565ea45ede680e6c69602db06da1dc57c27a149161e1f80022fde485c516" async="async"></script>
 
     <script>window.BASE = '/';</script>
