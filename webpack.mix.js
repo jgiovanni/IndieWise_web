76d6b00968b5179bdda231d3ee221b26466acc17
Jerez Bain <jerezb31@gmail.com>
2018-02-08 04:00:39 -0500

    add mix file

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,13 +4,15 @@ mix.options({
     processCssUrls: false
 });
 
-mix.sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+  .version();
 
 mix.js('resources/assets/js/main.vue.js', 'public/js')
     .js('resources/assets/js/projects.vue.js', 'public/js')
     .js('resources/assets/js/users.vue.js', 'public/js')
     .js('resources/assets/js/resources.vue.js', 'public/js')
     .js('resources/assets/js/vendor.js', 'public/js')
+  .version()
     //.extract(['vue', 'jquery', 'scrollmagic', 'moment', 'aos', 'axios', 'video.js', 'tether-shepherd']);
 
 mix.version();
